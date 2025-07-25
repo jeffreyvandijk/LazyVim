4906df004636775c16a869c3cd9c919e8c60804b
Jeffrey Van Dijk <jvdijk90@gmail.com>
2025-07-25 12:41:00 +0200

    autocmds for ansible yml or yamls files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.yml", "*.yaml" },
+  callback = function()
+    vim.bo.filetype = "yaml.ansible"
+  end,
+})
